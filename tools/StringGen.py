d56d61255f8513cbbf0391797cadb032b69d3846
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-02 07:44:55 +0700

    Update StringGen.py

--- tools/StringGen.py
+++ tools/StringGen.py
@@ -25,13 +25,13 @@ if path.isfile("config.env"):
 
 async def genStrSession() -> None:
     async with Client(
-        "ALPHA-Z",
+        "ALPHA",
         api_id=int(environ.get("API_ID") or input("Enter Telegram APP ID: ")),
         api_hash=environ.get("API_HASH") or input("Enter Telegram API HASH: "),
     ) as app:
         print("\nprocessing...")
         await app.send_message(
-            "me", f"#ALPHA-Z #HU_STRING_SESSION\n\n```{await app.export_session_string()}```"
+            "me", f"#ALPHA #HU_STRING_SESSION\n\n```{await app.export_session_string()}```"
         )
         print("Done !, session string has been sent to saved messages!")
 
